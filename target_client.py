d62ea057958652b7f77c15181d6cbf2859703209
Max-MI115 <max.murphy11@gmail.com>
2022-09-04 17:52:21 -0400

    Restore accidental deleted files

--- target_client.py
+++ target_client.py
@@ -5,24 +5,39 @@
 import asyncio
 import websockets
 import json
-import serial
+import sys
 
 IP = '128.2.244.29'
 PORT = 6789
 
-async def publisher():
-    uri = f'ws://{IP}:{PORT}'
-    async with websockets.connect(uri) as websocket:
-        with serial.Serial('/dev/ttyUSB0', 9600) as ser:
-            ser.readline()
-            while True:
-                line = ser.readline()
-                vals = line.decode()
-                # print(vals)
-                x, y = vals.split(',')
-                data = {'event': 'xy', 'x': int(x), 'y': int(y)}
-                await websocket.send(json.dumps(data))
-                print(data)
+def next_line(fname):
+    while True:
+        with open(fname, 'r') as file:
+            for line in file:
+                yield int(line)
+            close(file)
 
-asyncio.get_event_loop().run_until_complete(publisher())
+async def periodic_publisher(fname, uri):
+    next_target = next_line(fname)
+    async for websocket in websockets.connect(uri):
+        try:
+            message = await websocket.recv()
+            packet = json.loads(message)
+            try:
+                if packet['event'] == "get_tgt":
+                    tgt = next_target.next()
+                    data = {'event': 'set_tgt', 'tgt': tgt}
+                    await websocket.send(json.dumps(data))
+            except Exception:
+                pass                
+        except websockets.ConnectionClosed:
+                print("WS Connection Back-Pressure: Using New Websocket.")
+                continue
 
+if (len(sys.argv) < 2) or (sys.argv[1] is None):
+    name = 'targets.txt'
+    print("Looking for targets in default: targets.txt")
+else:
+    name = sys.argv[1]
+
+asyncio.get_event_loop().run_until_complete(periodic_publisher(name, f'ws://{IP}:{PORT}'))
